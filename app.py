42d902945a9f9336b158bc9fa718c7bf90b8e644
Blattellaza <hsieh.e.chen@gmail.com>
2024-12-08 21:30:43 +0800

    各個地方的排版

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
     # 血壓建議
     if isinstance(blood_pressure_in, float) and isinstance(blood_pressure_out, float) :
         if blood_pressure_in > 120  or blood_pressure_out > 80:
-            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out}，血壓太高！")
+            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out} mmHg")
+            fontLargestStrong['blood_pressure'].append("血壓太高(・-・ꐦ)！")
             fontSecondStrong['blood_pressure'].append("！注意！")
             fontNormalStrong['blood_pressure'].append("正常情況下，你的血壓應該為：")
-            fontNormalStrong['blood_pressure'].append("收縮壓90~120mmHg 和舒張壓60~80mmHg")
+            fontNormalStrong['blood_pressure'].append("收縮壓90~120mmHg 和舒張壓60~80 mmHg")
             fontSecondStrong['blood_pressure'].append("！改善！")
             fontNormalStrong['blood_pressure'].append("1.戒菸：戒菸有助健康，同時可減低發生心臟病及腦中風之機率。")
             fontNormalStrong['blood_pressure'].append("2.減重：維持理想體重，可減低體重過重所增加之心臟負荷。")
@@ -27,10 +28,11 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
             fontNormalStrong['blood_pressure'].append("4.控制飲酒：喝酒會使高血壓藥物失去療效。要避免血壓上升，飲酒量不宜超過30公克酒精。")
             fontNormalStrong['blood_pressure'].append("5.規律運動：每天30分鐘，一個星期最好五次以上，做一些安全溫和的有氧運動，可以改善血壓過高問題。")
         elif blood_pressure_in < 90 or  blood_pressure_out < 60:
-            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out}，血壓太低！")
+            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out} mmHg")
+            fontLargestStrong['blood_pressure'].append("血壓太低(・-・ꐦ)！")
             fontSecondStrong['blood_pressure'].append("！注意！")
             fontNormalStrong['blood_pressure'].append("正常情況下，你的血壓應該為：")
-            fontNormalStrong['blood_pressure'].append("收縮壓90~120mmHg 和舒張壓60~80mmHg")
+            fontNormalStrong['blood_pressure'].append("收縮壓90~120 mmHg 和舒張壓60~80mmHg")
             fontSecondStrong['blood_pressure'].append("！改善！")
             fontNormalStrong['blood_pressure'].append("1.多喝水：增加血容量、避免脫水造成低血壓，尤其在天氣炎熱時更要注意。")
             fontNormalStrong['blood_pressure'].append("2.避免處在悶熱的環境：容易使血管舒張、血壓下降。")
@@ -40,35 +42,39 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
         elif blood_pressure_in > 120 and blood_pressure_out < 60 or blood_pressure_in < 90 and blood_pressure_out > 80:
             fontLargestStrong['blood_pressure'].append("資訊輸入錯誤")
         else:
-            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out}，血壓正常")
+            fontLargestStrong['blood_pressure'].append(f"你的血壓為{blood_pressure_in}/{blood_pressure_out} mmHg")
+            fontLargestStrong['blood_pressure'].append("血壓正常٩(*´ ꒳ `*)۶")
             fontSecondStrong['blood_pressure'].append("！太棒了！")
             fontNormalStrong['blood_pressure'].append("正常情況下的血壓應該為：")
-            fontNormalStrong['blood_pressure'].append("收縮壓90~120mmHg 和舒張壓60~80mmHg")
+            fontNormalStrong['blood_pressure'].append("收縮壓90~120 mmHg 和舒張壓60~80mmHg")
+            fontNormalStrong['blood_pressure'].append("PS.也要定期回來做檢測喔 ⑉˙ᗜ˙⑉ ")
 
     else:
-        fontLargestStrong['blood_pressure'].append("沒有輸入收縮壓及舒張壓")
+        fontLargestStrong['blood_pressure'].append("（沒有輸入收縮壓及舒張壓）")
         fontSecondStrong['blood_pressure'].append("！注意！請完整輸入數據以獲得準確的建議！")
-        fontNormalStrong['blood_sugar'].append("PS.也要定期回來做檢測喔 ⑉˙ᗜ˙⑉ ")
+        
 
     # 血糖建議
     if isinstance(blood_sugar, float):
         if blood_sugar > 140:
-            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar}，血糖太高！")
+            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar} mg/dL")
+            fontLargestStrong['blood_sugar'].append("血糖太高(・-・ꐦ)！")
             fontSecondStrong['blood_sugar'].append("！注意！")
             fontNormalStrong['blood_sugar'].append("正常情況下，你的血糖應該為：")
-            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100mg/dL")
-            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140mg/dL")
+            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100 mg/dL")
+            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140 mg/dL")
             fontSecondStrong['blood_sugar'].append("！改善！")
             fontNormalStrong['blood_sugar'].append("1. 避免吃高升糖指數的食物，選擇 GI 值低於 55 的低升糖指數食物，例如：糙米、燕麥、山藥、大部分蔬菜、豆魚蛋肉類、小番茄、芭樂、木瓜等。")
             fontNormalStrong['blood_sugar'].append("2. 避免吃含精緻糖的食物，像是冰糖、砂糖、高果糖漿等精製糖，避免食用麵包、冰淇淋、甜甜圈等含精緻糖的食物。")
             fontNormalStrong['blood_sugar'].append("3. 避免吃高油脂食物，選擇植物油來替代動物性脂肪（如：豬油、牛油、奶油、培根等）。")
             fontNormalStrong['blood_sugar'].append("4. 避免吃高鈉食物，並避免過度飲酒。")
         elif blood_sugar < 70:
-            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar}，血糖太低！")
+            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar} mg/dL")
+            fontLargestStrong['blood_sugar'].append("血糖太低(・-・ꐦ)！")
             fontSecondStrong['blood_sugar'].append("！注意！")
             fontNormalStrong['blood_sugar'].append("正常情況下，你的血糖應該為：")
-            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100mg/dL")
-            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140mg/dL")
+            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100 mg/dL")
+            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140 mg/dL")
             fontNormalStrong['blood_sugar'].append("請注意您身理上是否有虛弱、嗜睡、飢餓、臉色蒼白、冒冷汗、心跳加快、發冷、抽筋、頭暈等症狀；心理上可能會有情緒改變及行為改變。")
             fontSecondStrong['blood_sugar'].append("！改善！")
             fontNormalStrong['blood_sugar'].append("以下為可能造成低血糖的原因：")
@@ -77,21 +83,23 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
             fontNormalStrong['blood_sugar'].append("運動：空腹運動、過度運動等。")
             fontNormalStrong['blood_sugar'].append("如果長時間處於低血糖狀態，建議就醫。")
         else:
-            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar}，血糖正常")
+            fontLargestStrong['blood_sugar'].append(f"你的血糖為{blood_sugar} mg/dL")
+            fontLargestStrong['blood_sugar'].append("血糖正常٩(*´ ꒳ `*)۶")
             fontSecondStrong['blood_sugar'].append("！太棒了！")
             fontNormalStrong['blood_sugar'].append("正常情況下的血糖應該為：")
-            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100mg/dL")
-            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140mg/dL")
+            fontNormalStrong['blood_sugar'].append("空腹血糖：70~100 mg/dL")
+            fontNormalStrong['blood_sugar'].append("飯後兩小時血糖：70~140 mg/dL")
             fontNormalStrong['blood_sugar'].append("PS.也要定期回來做檢測喔 ⑉˙ᗜ˙⑉ ")
     else:
-        fontLargestStrong['blood_sugar'].append("沒有血糖資料")
+        fontLargestStrong['blood_sugar'].append("（沒有血糖資料）")
         fontSecondStrong['blood_sugar'].append("！注意！請完整輸入數據以獲得準確的建議！")
 
     # BMI 建議
     if isinstance(weight, float) and isinstance(height, float):
         bmi = round(weight / (height / 100) ** 2, 2)
         if bmi >= 27:
-            fontLargestStrong['bmi'].append(f"你的BMI為{bmi}，肥胖！")
+            fontLargestStrong['bmi'].append(f"你的BMI為{bmi} kg/m\u00B2")
+            fontLargestStrong['bmi'].append("肥胖(・-・ꐦ)！")
             fontSecondStrong['bmi'].append("！注意！")
             fontNormalStrong['bmi'].append("健康的BMI應該為：18.5～24 kg/m\u00B2")
             fontNormalStrong['bmi'].append("肥胖會增加心血管疾病、糖尿病等慢性病風險，請立即採取行動！")
@@ -101,7 +109,8 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
             fontNormalStrong['bmi'].append("3.增加有氧運動（如快走、慢跑或騎自行車），每週至少進行150分鐘，幫助燃燒熱量。")
             fontNormalStrong['bmi'].append("4.每餐適量控制，避免暴飲暴食，並養成規律的飲食習慣，避免宵夜。")
         elif 24 <= bmi < 27:
-            fontLargestStrong['bmi'].append(f"你的BMI為{bmi}，過重！")
+            fontLargestStrong['bmi'].append(f"你的BMI為{bmi} kg/m\u00B2")
+            fontLargestStrong['bmi'].append("過重(・-・ꐦ)！")
             fontSecondStrong['bmi'].append("！注意！")
             fontNormalStrong['bmi'].append("健康的BMI應該為：18.5～24 kg/m\u00B2")
             fontNormalStrong['bmi'].append("體重過重可能帶來健康問題，建議調整生活方式。")
@@ -111,16 +120,18 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
             fontNormalStrong['bmi'].append("3.每天堅持運動，如散步、快走、爬樓梯等，逐步養成運動習慣。")
             fontNormalStrong['bmi'].append("4.避免久坐，建議每隔一段時間起身活動，讓身體保持活力。")
         elif 18.5 <= bmi < 24:
-            fontLargestStrong['bmi'].append(f"你的BMI為{bmi}，健康體重！")
+            fontLargestStrong['bmi'].append(f"你的BMI為{bmi} kg/m\u00B2")
+            fontLargestStrong['bmi'].append("健康體重٩(*´ ꒳ `*)۶")
             fontSecondStrong['bmi'].append("！太棒了！")
             fontNormalStrong['bmi'].append("健康的BMI應該為：18.5～24 kg/m\u00B2")
             fontNormalStrong['bmi'].append("雖然健康，但仍需保持良好的生活習慣！")
-            fontNormalStrong['bmi'].append("1.建議選擇均衡飲食，包含蛋白質、碳水化合物、健康脂肪以及維生素和礦物質，確保營養充足。！")
+            fontNormalStrong['bmi'].append("1.建議選擇均衡飲食，包含蛋白質、碳水化合物、健康脂肪以及維生素和礦物質，確保營養充足。")
             fontNormalStrong['bmi'].append("2.保持每週的運動頻率，如散步、瑜伽、慢跑等，讓身體保持健康活力。")
             fontNormalStrong['bmi'].append("3.定期檢查體重，以便及時調整生活習慣，防止體重波動。")
             fontNormalStrong['bmi'].append("PS.也要定期回來做檢測喔 ⑉˙ᗜ˙⑉ ")
         else:
-            fontLargestStrong['bmi'].append(f"你的BMI為{bmi}，過輕！")
+            fontLargestStrong['bmi'].append(f"你的BMI為{bmi} kg/m\u00B2")
+            fontLargestStrong['bmi'].append("過輕(・-・ꐦ)！")
             fontSecondStrong['bmi'].append("！注意！")
             fontNormalStrong['bmi'].append("健康的BMI應該為：18.5～24 kg/m\u00B2")
             fontNormalStrong['bmi'].append("體重過輕可能導致營養不良和免疫力下降，請關注健康！")
@@ -130,7 +141,7 @@ def get_health_advice(blood_pressure_in, blood_pressure_out, blood_sugar, height
             fontNormalStrong['bmi'].append("每天進行適量的力量訓練，如舉重或阻力訓練，增強肌肉，幫助體重增長。")
             fontNormalStrong['bmi'].append("確保攝取足夠的營養，多吃健康的點心，如水果乾、酸奶、花生醬夾心麵包等。")
     else:
-        fontLargestStrong['bmi'].append("沒有身高或體重資料")
+        fontLargestStrong['bmi'].append("（沒有身高或體重資料）")
         fontSecondStrong['bmi'].append("！注意！請完整輸入數據以獲得準確的建議！")
 
 
